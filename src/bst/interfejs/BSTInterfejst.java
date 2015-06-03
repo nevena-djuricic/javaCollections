9deecd960de58264a44624c8259e4458a824b616
nevena-djuricic <dj.nevena@hotmail.com>
2015-06-03 16:35:52 +0200

    Kreiran interfejs BST interfejs sa apstraktnim metodama koje definisu binatno stablo - ubaci, izbaci, visina, prefiks, infiks i postfiks

---
+++ src/bst/interfejs/BSTInterfejst.java
@@ -0,0 +1,12 @@
+package bst.interfejs;
+
+public interface BSTInterfejst {
+
+	public void ubaci(int vrednostNovogCvora);
+	public void izbaci(int vrednostCvoraZaIzbacivanje);
+	public int visina();
+	public void prefiks();
+	public void infix();
+	public void postfiks();
+	
+}
